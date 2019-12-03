dba4600fb0cdb179336ad155abb03fd2da11e875
2017080146 <56625851+joenduwind@users.noreply.github.com>
2019-12-03 23:37:00 +0800

    updates some files

--- app.js
+++ app.js
@@ -1,128 +1,64 @@
-//jshint esversion:8
-require("dotenv").config();
-const express = require("express");
-const bodyParse = require("body-parser");
-const bcrypt = require("bcryptjs");
-const passport = require("passport");
-const session = require("express-session");
-const flash = require("express-flash");
-const override = require("method-override");
-const app = express();
-const users = [];
-app.use(express.static("public"));
-app.use(bodyParse.urlencoded({ extended: false }));
-app.set("view engine", "ejs");
-const initializePassport = require("./passport-config");
-initializePassport(
-  passport,
-  email => users.find(user => user.email === email),
-  id => users.find(user => user.id === id)
-);
+// jshint esversion:8
+const express = require('express');
+const mongoose = require('mongoose');
+const passport = require('passport');
+const flash = require('connect-flash');
+const session = require('express-session');
 
-app.use(flash());
+const app = express();
+app.use(express.static("public"));
+// Passport Config
+require('./config/passport')(passport);
+
+// DB Config
+const db = require('./config/keys').mongoURI;
+
+// Connect to MongoDB
+mongoose
+  .connect(
+    db,
+    { useNewUrlParser: true, useUnifiedTopology: true }
+  )
+  .then(() => console.log('MongoDB Connected'))
+  .catch(err => console.log(err));
+
+// EJS
+app.set('view engine', 'ejs');
+
+app.get("/", (req, res)=>{
+  res.sendfile("index.html");
+});
+// Express body parser
+app.use(express.urlencoded({ extended: true }));
+
+// Express session
 app.use(
   session({
-    secret: process.env.SESSION_SECRET,
-    save: false,
+    secret: 'secret',
     resave: true,
-    saveUninitialized: false
+    saveUninitialized: true
   })
 );
-app.use(override("_method"));
+
+// Passport middleware
 app.use(passport.initialize());
 app.use(passport.session());
 
+// Connect flash
+app.use(flash());
 
-app.get("/", (req, res)=>{
-    res.sendFile(__dirname +"/index.html");
-});
-app.get("/about", (req, res) => {
-   res.sendFile(__dirname + "/about.html");
-});
-app.get("/cart", (req, res) => {
-   res.sendFile(__dirname + "/cart.html");
-});
-app.get("/checkout", (req, res) => {
-   res.sendFile(__dirname + "/checkout.html");
-});
-app.get("/contact-us", (req, res) => {
-   res.sendFile(__dirname + "/contact-us.html");
-});
-app.get("/my-account", (req, res) => {
-   res.sendFile(__dirname + "/my-account.html");
-});
-app.get("/service", (req, res) => {
-   res.sendFile(__dirname + "/service.html");
-});
-app.get("/shop-detail", (req, res) => {
-   res.sendFile(__dirname + "/shop-detail.html");
-});
-app.get("/shop", (req, res) => {
-  res.sendFile(__dirname + "/shop.html");
-});
-app.get("/wishlist", (req, res) => {
-  res.sendFile(__dirname + "/wishlist.html");
-});
-
-app.delete("/logout", (req, res) => {
-  req.logOut();
-  res.redirect("/login");
-});
-
-app.get("/register", canNotGoBackIfLoggedIn, (req, res) => {
-  res.render("register");
-});
-app.get("/login", canNotGoBackIfLoggedIn, (req, res) => {
-  res.render("login");
-});
-
-app.get("/user", canNotGoToUserWithOutLogIn, (req, res) => {
-  res.render("user", { users: users });
-});
-app.post(
-  "/login",
-  canNotGoBackIfLoggedIn,
-  passport.authenticate("local", {
-    successRedirect: "/user",
-    failureRedirect: "/login",
-    failureFlash: true,
-    successFlash: true
-  })
-);
-app.post("/register", canNotGoBackIfLoggedIn, async (req, res) => {
-  try {
-    const hashPass = await bcrypt.hash(req.body.password, 10);
-    users.push({
-      id: Date.now().toString(),
-      name: req.body.name,
-      email: req.body.email,
-      password: hashPass
-    });
-    res.redirect("/login");
-  } catch (error) {
-    res.redirect("/register");
-  }
-  console.log(users);
-});
-
-function canNotGoToUserWithOutLogIn(req, res, next) {
-  if (req.isAuthenticated()) {
-    return next();
-  }
-  res.redirect("/login");
-}
-
-function canNotGoBackIfLoggedIn(req, res, next) {
-  if (req.isAuthenticated()) {
-    return res.redirect("/user");
-  }
+// Global variables
+app.use(function(req, res, next) {
+  res.locals.success_msg = req.flash('success_msg');
+  res.locals.error_msg = req.flash('error_msg');
+  res.locals.error = req.flash('error');
   next();
-}
+});
 
+// Routes
+app.use("/", require("./routes/index.js"));
+app.use('/users', require('./routes/users.js'));
 
+const PORT = process.env.PORT || 3000;
 
-let port = process.env.PORT;
-if (port == null || port == "") {
-  port = 3000;
-}
-app.listen(port);
+app.listen(PORT, console.log(`Server started on port ${PORT}`));
---
+++ config/keys.js
@@ -0,0 +1,5 @@
+dbPassword = 'mongodb+srv://joelle:joelle@cluster0-gqhgk.mongodb.net/test?retryWrites=true&w=majority';
+
+module.exports = {
+    mongoURI: dbPassword
+};
